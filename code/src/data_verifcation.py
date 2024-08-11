b92b5873780adc2f16818628bef6bc7132b84ae2
Asif Sayyed <asifdotexe@gmail.com>
2024-08-11 14:00:32 +0530

    #11 PEP-8 formatted the data verfication script

--- code/src/data_verifcation.py
+++ code/src/data_verifcation.py
@@ -4,12 +4,21 @@ from skimage import io
 def verify_bounding_box(image_path, xmin, ymin, xmax, ymax):
     img = io.imread(image_path)
     fig = px.imshow(img)
+    
     fig.update_layout(
-        width=600, height=500, margin=dict(l=10, r=10, b=10, t=10),
+        width=600, 
+        height=500, 
+        margin=dict(l=10, r=10, b=10, t=10),
         xaxis_title='Bounding Box Verification'
     )
     fig.add_shape(
-        type='rect', x0=xmin, x1=xmax, y0=ymin, y1=ymax,
-        xref='x', yref='y', line_color='cyan'
+        type='rect', 
+        x0=xmin, 
+        x1=xmax, 
+        y0=ymin, 
+        y1=ymax,
+        xref='x', 
+        yref='y', line_color='cyan'
     )
+    
     fig.show()
